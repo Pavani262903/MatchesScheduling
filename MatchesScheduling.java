a9abb8c3800963e8b1afdc66814eac5c1cc7d9c0
Pavani262903 <125855611+Pavani262903@users.noreply.github.com>
2023-03-31 11:55:29 +0530

    Update MatchesScheduling.java

--- MatchesScheduling.java
+++ MatchesScheduling.java
@@ -1,50 +1,135 @@
-import java.util.Arraylist;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Scanner;
+ import java.util.*;
+   import java .util . stream.Collectors;
+   public class matches{
 
-public class Sample7 {
-    public static void main(String[] args) {
-        ArrayList<String> as = new ArrayList<>();
-   
-
-        as.add("team1");
-        as.add("team2");
-        as.add("team3");
-        as.add("team4");
-        as.add("team5");
-        ArrayList<Object> as2 = new ArrayList<>();
-        Match one = new Match(as.get(0));
-        Match two = new Match(as.get(1));
-        Match three = new Match(as.get(2));
-        Match four = new Match(as.get(3));
-        Match five = new Match(as.get(4));
-        as2.add(one);
-        as2.add(two);
-        as2.add(three);
-        as2.add(four);
-        as2.add(five);
-       
-          new Match(as2);
+   public static void main(String[] args) throws Exception {
+        ArrayList<group> teams=new ArrayList<>();
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println("enter the teams:");
+        for(int i=0;i<n;i++)
+        {
+            teams.add(new group(sc.next()));
         }
-    
-
- 
-}
-
-class Match {
-    String str;
-
-    Match(String str) {
-        this.str = str;
-    }
-    public String toString(){
-        return str;
-    }
-    Match(ArrayList<Object> as){
-      for(Object str2:as){
-        System.out.println(str2);
-
-      }
+        
+        List<game> matches=Scheduler.createSchedule(teams);
+        System.out.println(matches);
+        Simulator.directMatches(matches);
+        Simulator.showPointsTable(teams,matches);
     }
 }
+class game
+{
+    private group team1;
+    private group team2;
+    private group winner;
+    private group loser;
+    game(group team,group team3)
+    {
+        this.team1=team;
+        this.team2=team3;
+    }
+    public group getTeam1()
+    {
+        return team1;
+    }
+    public group getTeam2()
+    {
+        return team2;
+    }
+    public group getWinner()
+    {
+        return winner;
+    }
+    public void setWinner(group winner)
+    {
+        this.winner=winner;
+    }
+    public group getLoser()
+    {
+        return loser;
+    }
+    public void setLoser(group loser)
+    {
+        this.loser=loser;
+    }
+    @Override
+    public String toString()
+    {
+        String matchDescription = team1 +" vs "+team2;
+        if(winner!=null)
+        {
+            String result="\n winner="+this.winner.toString()+"Loser="+this.loser.toString();
+            matchDescription+=result;
+        }
+        return matchDescription;
+    }
+}
+class Scheduler
+{
+    public static List<game> createSchedule(List<group> teams)
+    {
+        List<game> matches=new ArrayList<>();
+        for(int i=0;i<teams.size();i++)
+        {
+            for(int j=i+1;j<teams.size();j++)
+            {
+                game match=new game(teams.get(i), teams.get(j));
+                matches.add(match);
+            }
+        }
+        return matches;
+    }
+}
+class Simulator
+{
+    /**
+     * @param matches
+     */
+    public static void directMatches(List<game> matches)
+    {
+        for(game match:matches)
+        {
+            int random=(int)((Math.random()*10)+1);
+            if(random%2==0)
+            {
+                match.setWinner(match.getTeam1());
+                match.setLoser(match.getTeam2());
+            }
+            else
+            {
+                match.setWinner(match.getTeam2());
+                match.setLoser(match.getTeam1());
+            }
+        }
+    }
+    public static void showPointsTable(List<group> teams,List<game> matches)
+    {
+        for(group team:teams)
+        {
+            int wonGames=matches.stream().filter(m ->m.getWinner().equals(team)).collect(Collectors.toList()).size();
+            int lostGames=matches.stream().filter(m ->m.getLoser().equals(team)).collect(Collectors.toList()).size();
+            System.out.println(team);
+            System.out.println(wonGames);
+            System.out.println(lostGames);
+        }
+    }
+}
+class group
+{
+    private String name;
+    group(String name)
+    {
+        this.name=name;
+    }
+    @Override
+    public String toString()
+    {
+        return name;
+    }
+    public boolean equals(Object obj)
+    {
+        return this.name.equals(((group)obj).name);
+    }
+}
+output:
